5f5310c2f9df56d90c0c4c0054bf5c7b26e9b3b6
jjeunv <124152742+jjeunv@users.noreply.github.com>
2023-05-25 02:57:24 +0900

    Solved Priority Queue practice problem

---
+++ W9_PriorityQueue/2023_P/2023_W9_P1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl "\n"
+
+class PriorityQueue {
+public:
+	vector<int> v;
+	
+	void insert(int value) {
+		for (int i = 0; i < v.size(); i++) {
+			if (value < v[i]) {
+				v.insert(v.begin()+i,value);
+				return;
+			}
+		}
+		v.push_back(value);
+	}
+
+	void print() {
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << " ";
+		}
+		cout << endl;
+		for (int i = 1; i < v.size(); i++) {
+			cout << v[0] + v[i] << " ";
+		}
+		cout << endl;
+	}
+};
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+	while (T--) {
+		PriorityQueue pq;
+		int N;
+		cin >> N;
+		while (N--) {
+			int n; cin >> n;
+			pq.insert(n);
+		}
+		pq.print();		
+	}
+}
---
+++ W9_PriorityQueue/2023_P/2023_W9_P2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl "\n"
+
+class PriorityQueue {
+public:
+	vector<int> first;
+	vector<int> second;
+	void insert(int value) {
+		if (value % 5 == 0) {
+			for (int i = 0; i < first.size(); i++) {
+				if (value > first[i]) {
+					first.insert(first.begin() + i, value);
+					return;
+				}
+			}
+			first.push_back(value);
+		}
+		else {
+			for (int i = 0; i < second.size(); i++) {
+				if (value > second[i]) {
+					second.insert(second.begin() + i, value);
+					return;
+				}
+			}
+			second.push_back(value);
+		}
+	}
+	
+	void print() {
+		for (int i = 0; i < first.size(); i++) {
+			cout << first[i] << " ";
+		}
+		for (int i = 0; i < second.size(); i++) {
+			cout << second[i] << " ";
+		}
+		cout << endl;
+	}
+};
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	int T;
+	cin >> T;
+	while (T--) {
+		PriorityQueue pq;
+
+		int N; cin >> N;
+
+		while (N--) {
+			int n; cin >> n;
+			pq.insert(n);
+		}
+		pq.print();
+	}
+}
